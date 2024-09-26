0baa3821b8d3b5ace3acda1cb793990ec37cdf8f
takuma <>
2024-09-26 13:32:53 +0900

    feat: prismaからpostを取得する

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,10 @@
+import { prisma } from "~/db.server";
+
 interface Post {
-  slug: string
-  title: string
+  slug: string;
+  title: string;
 }
 
 export async function getPosts(): Promise<Post[]> {
-  return [
-    {
-      slug: 'my-first-post',
-      title: 'my-first-post',
-    },
-    {
-      slug: 'my-first-post2',
-      title: 'my-first-post2',
-    },
-  ]
+  return prisma.post.findMany();
 }
