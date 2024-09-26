3f1c52f1864fe2b5bae8dde5adc42a55732c1c14
takuma <>
2024-09-26 14:24:22 +0900

    feat: prismaからpost詳細を取得

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -8,3 +8,11 @@ interface Post {
 export async function getPosts(): Promise<Post[]> {
   return prisma.post.findMany();
 }
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({
+    where: {
+      slug,
+    },
+  });
+}
--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,10 +1,27 @@
+import { json, LoaderFunctionArgs } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import invariant from "tiny-invariant"
+import { getPost } from "~/models/post.server"
+
+export const loader = async ({params} : LoaderFunctionArgs) => {
+  const slug = params.slug
+  invariant(slug, 'slug is required')
+  const post = await getPost(slug)
+  invariant(post, 'post is not found')
+  const markdown = post.markdown
+
+  return json({ post, markdown })
+}
+
 export default function PostSlug () {
+  const {post, markdown} = useLoaderData<typeof loader>()
+
   return (
     <main className="mx-auto max-w-4xl">
-      <h1>title</h1>
+      <h1>{post.title}</h1>
 
       <div>
-        markdown contents
+        {markdown}
       </div>
     </main>
   )
